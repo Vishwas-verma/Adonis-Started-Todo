fbab86ea4ca580091363a63cf183d0f54bd0d10d
Vishwas-verma <vermavarun932@gmail.com>
2021-05-22 01:22:53 +0530

    [Model Migrations]

---
+++ app/Models/Todo.ts
@@ -0,0 +1,27 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Todo extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public title: string
+
+  @column()
+  public is_created: string
+
+  @column()
+  public created_by: number
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @column.dateTime({ serializeAs:null })
+  public deletedAt: DateTime
+
+  // Todo Add Relationship
+}
---
+++ database/migrations/1621624546876_todos.ts
@@ -0,0 +1,20 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Todos extends BaseSchema {
+  protected tableName = 'todos'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').unique().primary()
+      table.string('title')
+      table.boolean('is_created').defaultTo(false)
+      table.integer('created_by')
+      table.dateTime("deleted_at").defaultTo(null)
+      table.timestamps(true)
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
